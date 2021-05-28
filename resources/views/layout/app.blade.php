d30845c7da54a3ad4c445b7c7b8f304f43a0b774
jonathanilunga <jonathanilunga84@gmail.com>
2021-05-28 22:33:11 +0100

    configuration de template de  base

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Google Fonts -->
+        <!--link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" -->
+        
+        <!-- Vendor CSS Files -->
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('vendors/icofont/icofont.min.css') }}" rel="stylesheet">
+
+        <!-- Style Css -->
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        
+        <title>@yield('title')</title>
+        <style>
+           
+        </style>        
+    </head>
+    <body>
+        <div class="container-fluid">
+            @include('layout.navigation')
+        </div>
+
+        <div id="imgHead" class="container-fluid">
+            @yield('contenu')
+        </div>
+
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+        <!-- Template Main JS File -->
+        <script src="{{asset('js/main.js')}}"></script>
+
+        <div class="container-fluid">
+            @include('layout.footer')
+        </div>
+    </body>
+</html>
---
+++ resources/views/layout/navigation.blade.php
@@ -0,0 +1,54 @@
+<!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <i class="icofont-envelope"></i> <a href="mailto:contact@example.com">fondationigwabi@gmail.com</a>
+        <i class="icofont-phone"></i> +243 81 174 8411
+      </div>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="#" class="skype"><i class="icofont-skype"></i></a>
+        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      </div>
+    </div>
+  </div>
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href="index.html">FondationdIgwabi<span>.</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt=""></a>-->
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="{{route('home')}}">Accueil</a></li>
+          <li><a href="#about">Mission</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Appropo</a></li>
+          <!--li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Deep Drop Down</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li-->
+          <li><a href="#contact">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+</header><!-- End Header -->
